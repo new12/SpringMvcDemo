b9c40cd28dff6aa8f2aa4cd14455cb1140f44d39
new12 <happynew_12@163.com>
2016-04-18 00:22:59 +0800

    save

--- src/main/java/configuration/SecurityConfig.java
+++ src/main/java/configuration/SecurityConfig.java
@@ -3,8 +3,10 @@ package configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 
 /**
  * Created by Administrator on 2016/4/16.
@@ -14,6 +16,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
    public void configureGlobal(AuthenticationManagerBuilder auth) throws  Exception{
-       auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+       auth
+       .inMemoryAuthentication().withUser("user").password("password").roles("USER");
    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	http.authorizeRequests().antMatchers("/static/**").permitAll()
+    		.anyRequest().authenticated()
+    	.and()
+    		.formLogin()
+    		.loginPage("/login")
+    		.permitAll();
+    	
+    }
 }
