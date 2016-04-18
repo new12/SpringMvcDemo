b59daa999ac323dc7a7dc18425698286cfe6466e
new12 <happynew_12@163.com>
2016-04-18 16:06:10 +0800

    fix

--- src/main/java/configuration/WebConfig.java
+++ src/main/java/configuration/WebConfig.java
@@ -3,6 +3,7 @@ package configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
